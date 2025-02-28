1707f339f51055d361b3e23db4c0db723c167fd4
trantiensinh <tranngocminhqb@gmail.com>
2025-02-28 15:30:49 +0700

    3.7 Demo Using Postman

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    public class CitiesController
+    {
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new { id = 1, name = "New York City"},
+                    new { id = 2, name = "Antwerp"}
+                });
+        }
+    }
+}
