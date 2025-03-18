aa22be0dea975ef5b1c83eeb3ac4f50019e91bd1
trantiensinh <tranngocminhqb@gmail.com>
2025-03-18 16:31:32 +0700

    4.9) Demo: Partially Updating a Resource

--- CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddde2562042d2cf5664581f63dbacdc4639338db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87834492e38f7ea74b5fb0f391b899af1fe3dd94")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
